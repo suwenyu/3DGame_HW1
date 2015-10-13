2f9ecf5e14ec6b83d07aa0f4b77eb79d50b54496
suwenyu <cy_i90118@yahoo.com.tw>
2015-10-13 15:19:30 +0800

    Initial Commit

---
+++ HW1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW1
+//
+//  Created by WenYu on 2015/10/13.
+//  Copyright © 2015年 WenYu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
